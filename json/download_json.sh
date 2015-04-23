65ae109588c60bd88f2d8cbaa7d48799b9424627
Andrew Martin <a.a.d.martin@gmail.com>
2015-04-24 09:15:17 +1000

    Added pull data func to data json downloader

--- json/download_json.sh
+++ json/download_json.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
 CMDDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-SERVICES=("ec2" "s3")
+SERVICES=("ec2")
 
+scrape(){
+
+  SERVICE=$1
+
+  # Create service directories is they dont exist
+  if [ ! -d "${CMDDIR}/${SERVICE}" ]; then
+    mkdir "${CMDDIR}/${SERVICE}"
+  fi
+
+  # Get all json URLs from the pricing page
+  for url in `curl http://aws.amazon.com/${SERVICE}/pricing/ 2>/dev/null | grep 'model:' | sed -e "s/.*'\(.*\)'.*/http:\\1/"`;
+  do
+      wget --directory-prefix="${CMDDIR}/${SERVICE}" $url
+  done
+
+}
 
 
 
